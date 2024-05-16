a94eafc2b938538a62cbb4cb2085e46f0e9e9cb7
Roberto Londoño <roberthdrums@gmail.com>
2024-05-16 12:37:50 -0500

    language picker

---
+++ src/i18n/ui.ts
@@ -0,0 +1,18 @@
+export const languages = {
+  es: 'Spanish',
+  en: 'English'  
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+  es: {
+    'nav.home': 'Home',
+    'nav.about': 'Sobre mí',
+  },
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.twitter': 'Twitter',
+  }  
+} as const;
